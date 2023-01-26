b00fbc3063d0a89f563a5b696a3cc5f93e5a34db
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-01-26 17:05:28 +0100

    add html email

--- email/main.js
+++ email/main.js
@@ -2,9 +2,12 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
 
-const emailUser = prompt("digiti la sua email")
-// console.log(emailUser);
+// input element
+const inputButtonEl = document.getElementById("button_send");
+const inputEmailUserEl = document.getElementById("email_user");
+const welcomeMessageEl = document.getElementById("welcome_user");
 
+// email list
 const emailList = [
     "mariorossi@gmail.com",
     "gigibiachi@gmail.com",
@@ -12,20 +15,34 @@ const emailList = [
     "gigiferri@gmail.com",
 ]
 
-let emailFound = false;
 
-for (let i = 0; i < emailList.length; i++) {
-    const currentEmail = emailList[i]
+inputButtonEl.addEventListener(
+    "click",
+    function () {
+        let emailFound = false;
+        let outputMessage;
+        const emailUser = inputEmailUserEl.value;
 
-    if (emailUser === currentEmail) {
-        emailFound = true;
+        for (let i = 0; i < emailList.length; i++) {
+            const currentEmail = emailList[i];
+
+            if (emailUser === currentEmail) {
+                emailFound = true;
+            }
+        }
+
+        if (emailFound) {
+            outputMessage = `Benvenuto, ${emailUser}`;
+        }
+        else {
+            outputMessage = `Accesso negato, inserire una nuova email`;
+        }
+
+        welcomeMessageEl.innerHTML = outputMessage;
     }
-}
 
-if (emailFound) {
-    console.log(`benvenuto${emailUser}`);
-}
-else {
-    console.log(`accesso negato`);
-}
+)
+
+
+
 
